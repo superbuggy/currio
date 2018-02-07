a2fd3b392a92da9a14ff99999aa2e41515d94626
superbuggy <jpqreichard@gmail.com>
2018-02-07 12:10:04 -0500

    add bookmarks file

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 const express = require('express')
 const open = require('opn')
 const app = express()
+const sites = require('./sites.json')
 
 const PORT = 3004
 
 app.set('port', PORT)
 
-const sites = require('./sites.json')
-app.get('/', (request, response) => {
-  response.status(200).json(sites)
-})
+app.get('/', (request, response) => response.status(200).json(sites))
 
-app.listen(app.get('port'), _ => {
-  open(`http://localhost:${app.get('port')}`)
-})
+app.listen(app.get('port'), _ => open(`http://localhost:${app.get('port')}`))
