cabf1e7aa6ef61570fe6ae334cd31e662930f25a
superbuggy <jpqreichard@gmail.com>
2018-02-07 13:20:29 -0500

    use bookmarks parsing for categorized bookmarks

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 const express = require('express')
 const open = require('opn')
-const sites = require('./sites.json')
 const bookmarks = require('bookmarks-to-json-categorization')
 const app = express()
 const sites = require('./sites.json')
 
 const PORT = 3004
+const BOOKMARK_FILENAME = 'bookmarks_2_7_18.html' 
 
 app.set('port', PORT)
 
 
-bookmarks(
-  'bookmarks.html', 
-  options = { pipeFile: 'bookmarks.json', exportToFile: true}, 
-  function(data){
-    console.log('bookmarks', data)
-  }
-);
-
-
 app.get('/', (request, response) => {
-  response.status(200).json(sites)
+  bookmarks(
+    BOOKMARK_FILENAME, 
+    options = { pipeFile: 'bookmarks.json', exportToFile: true}, 
+    function (data) {
+      console.log('bookmarks', data)
+      response.status(200).json(JSON.parse(data))
+    }
+  );
 })
+
+
 
 app.listen(app.get('port'), _ => open(`http://localhost:${app.get('port')}`))
