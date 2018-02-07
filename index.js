ae641a9f5995bc5dba77224bdb5b584c1107e912
superbuggy <jpqreichard@gmail.com>
2018-02-07 12:24:18 -0500

    resolve merge stash pop

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const open = require('opn')
+const sites = require('./sites.json')
+const bookmarks = require('bookmarks-to-json-categorization')
 const app = express()
 const sites = require('./sites.json')
 
@@ -7,6 +9,18 @@ const PORT = 3004
 
 app.set('port', PORT)
 
-app.get('/', (request, response) => response.status(200).json(sites))
+
+bookmarks(
+  'bookmarks.html', 
+  options = { pipeFile: 'bookmarks.json', exportToFile: true}, 
+  function(data){
+    console.log('bookmarks', data)
+  }
+);
+
+
+app.get('/', (request, response) => {
+  response.status(200).json(sites)
+})
 
 app.listen(app.get('port'), _ => open(`http://localhost:${app.get('port')}`))
